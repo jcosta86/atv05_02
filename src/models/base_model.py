128b52ade2479f4cb6c2b2a286a87ddd5de82b51
victorhausen <hausen.victor@gmail.com>
2021-02-05 11:57:24 -0300

    implementing models

---
+++ src/models/base_model.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, Column
+
+
+Base = declarative_base()
+
+class BaseModel(Base):
+    __abstract__ = True
+    id_ = Column(Integer, primary_key=True)
---
+++ src/models/product.py
@@ -0,0 +1,47 @@
+from sqlalchemy import String, DECIMAL, Column
+from sqlalchemy.orm import validates
+
+from src.models.base_model import BaseModel
+
+
+class Product(BaseModel):
+    __tablename__="PRODUCT_GRUPO_5"
+    name = Column(String(length=50), nullable=False)
+    description = Column(String(length=500), nullable=False)
+    price = Column(DECIMAL, nullable=False)
+
+    def __init__(self, name: str, description: str, price: float) -> None:
+        self.name = name
+        self.description = description
+        self.price = price
+
+    @validates("name")
+    def validates_name(self, key: str, name: str) -> str:
+        if not isinstance(name, str):
+            raise TypeError, "Name must be str."
+        if len(name) > 50:
+            raise ValueError, "Name length must not be greater than 50"    
+        if name.strip():
+            raise ValueError, "Name must not be empty."
+
+        return name
+    
+    @validates("description")
+    def validates_name(self, key: str, description: str) -> str:
+        if not isinstance(description, str):
+            raise TypeError, "Description must be str."
+        if len(description) > 200:
+            raise ValueError, "Name length must not be greater than 200"    
+        if name.strip():
+            raise ValueError, "Name must not be empty."
+
+        return description
+
+    @validates("price")
+    def validates_name(self, key: str, price: float) -> str:
+        if not isinstance(price, float):
+            raise TypeError, "Price must be float."
+        if price > 0:
+            raise ValueError, "Price must be greater than 0."    
+        
+        return price
